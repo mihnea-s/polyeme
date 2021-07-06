271295b9e6a749c3fb176ee86b3aed2bea5ae623
Mihnea Stoian <mihn.stn@gmail.com>
2021-07-06 16:23:09 +0200

    Update browser api

--- typescript/source/platform/browser.ts
+++ typescript/source/platform/browser.ts
@@ -1,18 +1,51 @@
+/**
+ * Tiny makeshft stand-in for NodeJs APIs in the browser.
+ */
 
-export class ReadPort { }
+export abstract class ReadPort {
+  abstract read(): string;
+  abstract destroy(): string;
+}
 
-export class WritePort { }
+export abstract class WritePort {
+  abstract write(): string;
+  abstract destroy(): string;
+}
 
-export class ReadWritePort { }
+export abstract class ReadWritePort {
+  abstract read(): string;
+  abstract write(): string;
+  abstract destroy(): string;
+}
 
 class UnsupportedError { }
 
+let browserStdin: ReadPort | null = null;
+
 export function getStdin(): ReadPort {
-  throw new UnsupportedError();
+  if (!browserStdin) {
+    throw new UnsupportedError();
+  }
+
+  return browserStdin;
 }
 
-export function getStdout() {
-  throw new UnsupportedError();
+export function setStdin(stdin: ReadPort) {
+  browserStdin = stdin;
+}
+
+let browserStdout: WritePort | null = null;
+
+export function getStdout(): WritePort {
+  if (!browserStdout) {
+    throw new UnsupportedError();
+  }
+
+  return browserStdout;
+}
+
+export function setStdout(stdout: WritePort) {
+  browserStdout = stdout;
 }
 
 export function systemExec(_: string, __: string[]): [WritePort, ReadPort] {
